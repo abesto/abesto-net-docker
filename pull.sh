b7d78c30e8912353920520900614d89e91cd4e51
Zoltan Nagy <abesto@abesto.net>
2014-12-30 19:24:03 +0100

    Add are-you-board to pull.sh

--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repos='dockerfile/redis dockerfile/haproxy abesto/blog abesto/abesto-net-haproxy'
+repos='dockerfile/redis dockerfile/haproxy abesto/blog abesto/abesto-net-haproxy abesto/are-you-board'
 for repo in $repos; do
 	docker pull $repo
 done
